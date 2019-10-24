f9c54027f4739f7cfff909100b034df2e9d9e1e2
Sicheng Zeng <zengcs@stanford.edu>
2019-10-24 00:13:29 -0700

    Name fixed in asm/main.c

--- assign5/program/asm/main.c
+++ assign5/program/asm/main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-extern int mystery(int);
+extern int _mystery(int);
 
 int main() {
-  printf("3: %d\n", mystery(3));
-  printf("1: %d\n", mystery(1));
-  printf("100: %d\n", mystery(100));
+  printf("3: %d\n", _mystery(3));
+  printf("1: %d\n", _mystery(1));
+  printf("100: %d\n", _mystery(100));
   return 0;
 }
