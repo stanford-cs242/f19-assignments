90b6d3172a02713fafac73a04f15a9cb8a7aeea6
Will Crichton <wcrichto@cs.stanford.edu>
2019-11-15 10:53:17 -0800

    Remove last year's code from a8

--- assign8/program/src/lib.rs
+++ assign8/program/src/lib.rs
@@ -1,8 +1,5 @@
 extern crate rand;
 
-// UNCOMMENT this when doing section 1, and RE-COMMENT it when doing section 2
-//mod counterex;
-
 // UNCOMMENT this when doing section 2
 // pub mod tcp;
 mod backend;
